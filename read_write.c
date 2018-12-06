c38dc59b247cb84ddfa462b1c7b8c735e3bb8b15
ryanmanny <ryan.manny@wsu.edu>
2018-12-05 22:15:07 -0800

    Implement write

--- read_write.c
+++ read_write.c
@@ -12,7 +12,7 @@ int my_read(int fd, char buf[], int nbytes)
     
     file->refCount++;
 
-    int count = 0;
+    int count        = 0;
 
     int size         = file->mptr->INODE.i_size;
     
@@ -57,10 +57,63 @@ int my_read(int fd, char buf[], int nbytes)
         }
     }
 
+    file->refCount--;
+
     return count;  // Return actual number of bytes read
 }
 
 int my_write(int fd, char buf[], int nbytes)
 {
+    char writebuf[BLKSIZE], *cp;
+
+    OFT    *file = running->fd[fd];
+    MINODE *mip  = file->mptr;
     
+    file->refCount++;
+
+    int count        = 0;
+
+    int start_block  = file->offset / BLKSIZE;
+    int start_byte   = file->offset % BLKSIZE;
+    int remain_block = BLKSIZE - start_byte;    // Number of bytes free in this block
+
+    int i_block      = start_block;             // Block index
+    int cur_block    = 0;                       // Actual block #
+
+    int min;
+
+    while (nbytes)
+    {
+        min = nbytes;                           // Number of bytes to read
+        if (remain_block < min)
+            min = remain_block;
+
+        cur_block = get_ith_block(mip, i_block);
+        get_block(mip->dev, cur_block, writebuf);
+
+        cp = writebuf + start_byte;
+        start_byte = 0;
+
+        memcpy(buf, cp, min);
+
+        put_block(mip->dev, cur_block, writebuf);
+
+        buf += min;
+        count += min;
+        file->offset += min;
+
+        nbytes -= min;
+        remain_block -= min;
+
+        if (remain_block == 0)
+        {
+            i_block++;                           // Move to next block
+            remain_block = BLKSIZE;
+        }
+    }
+
+    file->refCount--;
+    mip->dirty = 1;                              // Mark mip dirty fucker       
+
+    return count;
 }
